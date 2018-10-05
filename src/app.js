dceaaeaf3991e4823acdb909d5fd423e966ff0d7
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-10-05 15:48:26 -0400

    Complete ZipCodeForm and CityCard components

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import './styles.css';
 
-// App component
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Banner />
-        <Field />
-        <CityCard />
-      </div>
-    );
-  }
-}
 
 // Banner component
 class Banner extends React.Component {
@@ -25,20 +13,96 @@ class Banner extends React.Component {
   }
 }
 
-class Field extends React.Component {
+class ZipCodeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetching: true,
+      cityInfo: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  fetchZipCodeInfo(zipCode) {
+    // fetch zip code information
+    if(zipCode.length === 5) {
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${zipCode}`)
+        .then(function(response) {
+          return response.json();
+        }).then(function(json) {
+          const array = json.map(function(city) {
+            return {
+              locationText: city.LocationText,
+              state: city.State,
+              location: `(${city.Lat}, ${city.Long})`,
+              population: city.EstimatedPopulation,
+              totalWages: city.TotalWages,
+            }});
+          return array;
+        }).then(function(zipInfo) {
+          console.log(zipInfo);
+          const cards = zipInfo.map(city => <CityCard data={city} />);
+          this.setState({
+            cityInfo: cards,
+          });
+        }).catch(function(ex) {
+          console.log(ex);
+          return "Cannot Resolve";
+        });
+    }
+  }
+
+  handleChange(event) {
+    this.fetchZipCodeInfo(event.target.value);
+  }
+
   render() {
     return (
-      <form>
-        <label>Zip Code:</label>
-        <input type="text"></input>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+          <form className="zipform">
+            <label>
+              <b>Zip Code: </b>
+            </label>
+            <input type="text" placeholder={"try 10001"} onChange={this.handleChange} />
+          </form>
+      </div>
     );
   }
 }
 
 class CityCard extends React.Component {
   render() {
-    return <p>City Info Goes Here</p>;
+    // render city information
+    const { data } = this.props;
+
+    return (
+      <div>
+        <div className="citycard">
+          <div className="citycardheader">{data.locationText}</div>
+          <div>
+            <ul>
+              <li>State: {data.state}</li>
+              <li>Location: {data.location}</li>
+              <li>Population (estimated): {data.population}</li>
+              <li>Total Wages: {data.totalWages}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// App component
+export class App extends React.Component {
+  // main app component
+  render() {
+    return (
+      <div>
+        <Banner />
+        <ZipCodeForm />
+      </div>
+    );
   }
 }
